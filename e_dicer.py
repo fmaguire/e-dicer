5ae27fe5bec10d7c85891081b3329866cad3b93a
Finlay Maguire <finlaymaguire@gmail.com>
2014-10-21 20:25:28 +0100

    Develop fragmentation function
    * Write unit tests for fragmentation function
    * Write fragmentaion function using TDD

--- e_dicer.py
+++ e_dicer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+import Bio
 from Bio import SeqIO
 import os.path
+import warnings
 
 def parse_fasta(fasta_file_name):
     '''
@@ -11,8 +13,61 @@ def parse_fasta(fasta_file_name):
     '''
 
     if not os.path.isfile(fasta_file_name):
-        raise IOError
+        raise IOError("{0} can't be found".format(fasta_file_name))
 
-    seq_generator = SeqIO.parse(fasta_file_name, 'fasta')
-
+    seq_generator = Bio.SeqIO.parse(fasta_file_name, "fasta")
     return seq_generator
+
+def generate_fragments(seqrec, k=21):
+    '''
+    Function to generate all possible contiguous k-length fragments of a
+    specific seq
+    input:   k - fragment size default is 21bp
+           seq - seqrecord containing seq for fragmentation
+    output: seq_fragments - list of seqrecords containing all the fragments
+    '''
+
+    #validate input
+    if type(k) is not int or not int(k) > 0:
+        raise ValueError("K must be an int > 0, "\
+                         "'{0}' is thus invalid".format(str(k)))
+
+    if type(seqrec) is not Bio.SeqRecord.SeqRecord:
+        raise ValueError("seqrec must be a single SeqRecord obj, "\
+                         "'{0}' is thus invalid".format(str(seqrec)))
+
+    seq_len = len(seqrec.seq)
+
+    if k > seq_len:
+        # raise warning here if sequence is shorter than k
+        warnings.warn("Sequence (seqrec={0}) shorter than k (k={1}) "\
+                      "therefore discarding".format(seqrec.id, k),
+                      UserWarning)
+        return None
+
+    elif seq_len >= k:
+        #generate all fragments
+        fragments = [Bio.SeqRecord.SeqRecord(seq=seqrec.seq[x: x + k],
+                                             name=seqrec.name,
+                                             id=seqrec.id,
+                                             description=seqrec.description,
+                                             dbxrefs=[]) \
+                     for x in range(seq_len) if not x+k > seq_len]
+
+        #add identifer information to fragment
+        #this specified fragment size used and which fragment within the
+        #set generated the current fragment is
+        nfragments = len(fragments)
+        labelled_fragments = [Bio.SeqRecord.SeqRecord(\
+                                seq=fragments[x].seq,
+                                id=seqrec.id,
+                                name=fragments[x].name,
+                                description=\
+                                "{0} [{1}bp fragment {2} of {3}]"\
+                                "".format(fragments[x].description,
+                                          k,
+                                          x+1,
+                                          nfragments)) \
+                              for x in range(nfragments)]
+
+        return labelled_fragments
--- test/test.py
+++ test/test.py
@@ -10,11 +10,9 @@ import types
 import warnings
 import random
 
-from StringIO import StringIO
-from Bio import Alphabet
-from Bio import SeqRecord
-from Bio import Seq
+import Bio
 from Bio import SeqIO
+import StringIO
 
 class TestFastaParsing(unittest.TestCase):
     '''
@@ -32,7 +30,8 @@ class TestFastaParsing(unittest.TestCase):
         '''
         Test if fasta parser correctly raises IO error for non-existant fasta file
         '''
-        self.assertRaises(IOError, e_dicer.parse_fasta, u'NOT_A_REAL_FASTA.fasta')
+        self.assertRaisesRegexp(IOError ,"NOT_A_REAL_FASTA.fasta can't be found",
+                                eDicer.parse_fasta, u'NOT_A_REAL_FASTA.fasta')
 
     def test_generator_creation(self):
         '''
@@ -53,12 +52,103 @@ class TestFastaParsing(unittest.TestCase):
         self.assertEqual(len(self.seqs), 100)
 
         self.random_seq = random.sample(self.seqs, 1)[0]
-        self.assertIs(type(self.random_seq), SeqRecord.SeqRecord)
+        self.assertIs(type(self.random_seq), Bio.SeqRecord.SeqRecord)
 
     #todo: implement test (and feature) to ensure parsed input is nucl
     #      and not prot
 
+
+class TestDicerFunction(unittest.TestCase):
+    '''
+    Class to test the correct generation of all possible k-fragments
+    from a single SeqRecord
+    '''
+
+    def setUp(self):
+        '''
+        Parse and grab a random seq
+        '''
+        self.raw_seq = """>m.32354 g.32354  ORF g.32354 m.32354 \
+type:3prime_partial len:74 (+) \
+comp10036_c0_seq1:394-618(+)_
+ATGCAGATGCGTGTTCGAATGTACGTTGGTGGCG\
+TGCTGCTCGCGCTTCTGCTTGCGA"""
+        self.example_seq = Bio.SeqIO.parse(StringIO.StringIO(self.raw_seq), "fasta").next()
+
+        self.example_21_frag_file = "test/sample_fragments.fasta"
+
+        self.example_frags = [seqrecord for seqrecord in \
+                             Bio.SeqIO.parse(self.example_21_frag_file, "fasta")]
+
+        self.example_seq_len = len(self.example_seq.seq)
+
+    def test_sequence_too_short(self):
+        '''
+        Test when len(seq) < k function correctly discards seq with a warning
+        '''
+
+        with warnings.catch_warnings(record=True) as w:
+            self.short = eDicer.generate_fragments(self.example_seq,
+                                                   self.example_seq_len + 1)
+
+            self.assertIs(self.short, None)
+
+            self.assertEqual(len(w), 1)
+            self.assertIs(w[-1].category, UserWarning)
+            self.assertEqual(str(w[-1].message), "Sequence (seqrec={0}) shorter than "\
+                                            "k (k={1}) therefore "\
+                                            "discarding".format(self.example_seq.id,
+                                                                self.example_seq_len+1))
+
+    def test_sequence_same_length(self):
+        '''
+        Test when k==len(seq) function just returns the same sequence
+        '''
+        self.same = eDicer.generate_fragments(self.example_seq,
+                                              self.example_seq_len)
+
+        self.assertIsNot(self.same, None)
+        self.assertEqual(len(self.same), 1)
+        self.assertEqual(str(self.same[0].seq), str(self.example_seq.seq))
+        self.assertEqual(self.same[0].description, "{0} [{1}bp fragment" \
+                                                   " 1 of 1]".format(self.example_seq.description,
+                                                                     self.example_seq_len))
+
+
+    def test_sequence_fragmentation(self):
+        '''
+        Test for correct fragmentation of sample seq with default k (21)
+        '''
+
+        test_frags = eDicer.generate_fragments(self.example_seq)
+        for frag_index in range(len(self.example_frags)):
+            self.assertEqual(str(self.example_frags[frag_index].seq),
+                             str(test_frags[frag_index].seq))
+            self.assertEqual(self.example_frags[frag_index].description,
+                             test_frags[frag_index].description)
+
+
+    def test_incorrect_input(self):
+        '''
+        Test function raises exception if input isn't a seqrec and positive int
+        '''
+
+        self.randint = random.randint(-1000, 0)
+
+        self.assertRaisesRegexp(ValueError,
+                                "K must be an int > 0, '{0}' is thus "\
+                                "invalid".format(str(self.randint)),
+                                eDicer.generate_fragments, self.example_seq, self.randint)
+
+        self.assertRaisesRegexp(ValueError,
+                                "K must be an int > 0, 'a' is thus invalid",
+                                eDicer.generate_fragments, self.example_seq, 'a')
+
+        self.assertRaisesRegexp(ValueError,
+                                "seqrec must be a single SeqRecord obj, 'test'"\
+                                " is thus invalid",
+                                eDicer.generate_fragments, 'test', 54)
+
 if __name__=='__main__':
 
     unittest.main()
-
